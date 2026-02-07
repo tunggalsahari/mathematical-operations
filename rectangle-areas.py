a6abf85168e35e17c528f33868ad7ed3d99a4c4b
Tunggal Sahari <tunggalsahari1@gmail.com>
2026-02-07 17:37:23 +0700

    Create rectangle-areas.py
    A program that calculate the areas of a rectangle written in Python.

---
+++ rectangle-areas.py
@@ -0,0 +1,9 @@
+banner = (5*"="+" Square Area Finder "+5*"=")
+print(banner)
+
+length = float(input("=> Enter your rectangle length (in CM): "))
+height = float(input("=> Enter your rectangle height (in CM): "))
+result = length * height
+
+message = (f"=> The result is {result} CM^2")
+print(message)
