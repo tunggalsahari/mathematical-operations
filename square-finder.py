2cba4a8051deed1b6649ad68eb3fde8b30e9c456
Tunggal Sahari <tunggalsahari1@gmail.com>
2026-02-07 17:32:50 +0700

    Create square-finder.py
    A program that calculate the areas of a square written in Python.

---
+++ square-finder.py
@@ -0,0 +1,8 @@
+banner = (5*"="+" Square Area Finder "+5*"=")
+print(banner)
+
+sides = ("=> Enter your square sides (in CM): ")
+sides = float(input(sides))
+result = sides * sides
+
+message = (f"The result is {result} CM^2")
